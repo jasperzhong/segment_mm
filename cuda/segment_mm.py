3023805194f51895f66a39995d7c81cfeaa854d0
zhongyuchen <izhongyuchen@gmail.com>
2019-09-19 07:55:39 +0000

    UPDATE fix bugs in SegmentMMFunction

--- cuda/segment_mm.py
+++ cuda/segment_mm.py
@@ -7,11 +7,13 @@ import segment_mm
 class SegmentMMFunction(Function):
     @staticmethod
     def forward(ctx, mat_A, mat_B, segment_id_A, segment_id_B):
+        ctx.save_for_backward(mat_A, mat_B, segment_id_A, segment_id_B)
         return segment_mm.forward(mat_A, mat_B, segment_id_A, segment_id_B)
     
     @staticmethod
-    def backward(ctx, grad_c, mat_A, mat_B, segment_id_A, segment_id_B):
-        return segment_mm.backward(grad_c, mat_A, mat_B, segment_id_A, segment_id_B)
+    def backward(ctx, grad_c):
+        dA, dB = segment_mm.backward(grad_c.contiguous(), *ctx.saved_variables)
+        return dA, dB, None, None
 
 class SegmentMM(nn.Module):
     def __init__(self, ):
